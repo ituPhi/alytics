05a9fd7527dc863914462399b3220c5e27a123f5
i2phi <jfiturbe@gmail.com>
2025-05-07 11:17:52 -0500

    implemented compilerNode

--- graph.ts
+++ graph.ts
@@ -1,7 +1,7 @@
 import { Annotation, StateGraph } from "@langchain/langgraph";
 import { ChatOpenAI } from "@langchain/openai";
 import { externalChartConfig, externalGoals } from "./configs/config";
-import { createAnalizerAgent } from "./utils/agents";
+import { createAnalizerAgent, createCopyWriterAgent } from "./utils/agents";
 import { runAllReports, getGoals } from "./utils/data";
 import { GenerateSimpleChartNode } from "./utils/GenerateChart";
 
@@ -13,6 +13,7 @@ const StateAnnotation = Annotation.Root({
     {
       title: string;
       description: string;
+      url: string;
       buffer: Buffer;
     }[]
   >,
@@ -30,26 +31,33 @@ async function PrepareDataNode(
   };
 }
 
-async function AnalyzeNode(state: typeof StateAnnotation.State) {
-  //currently is turned off its not returning state
+async function AnalyzeNode(
+  state: typeof StateAnnotation.State,
+): Promise<typeof StateAnnotation.Update> {
   const { data, goals } = state;
-
   const llm = new ChatOpenAI({
     apiKey: process.env.OPENAI_KEY,
     model: "gpt-4.1-nano",
   });
   const analizer = await createAnalizerAgent({ llm: llm });
-  //const analisys = await analizer.invoke({ data: data, goals: goals });
-  //console.log(analisys.content);
+  const analysis = await analizer.invoke({ data: data, goals: goals });
+  const content = analysis.content;
+  let analysisObj = { content };
+  return {
+    analysis: content,
+  };
 }
 
 async function ChartsNode(state: typeof StateAnnotation.State) {
+  // this return PROMISE is NOT properly typed
   const chartConfigs = externalChartConfig; // chart config should eventualy be a call to supabase
   return {
     charts: await Promise.all(
       chartConfigs.map(async (config) => ({
         title: config.dataKey,
         description: config.description,
+        url: "https://placehold.co/600x600",
+        //hard coded for testing untill we set the upload
         buffer: await GenerateSimpleChartNode({
           data: state.data[config.dataKey],
           labelKey: config.labelKey,
@@ -62,18 +70,39 @@ async function ChartsNode(state: typeof StateAnnotation.State) {
   };
 }
 
+async function CompileNode(
+  state: typeof StateAnnotation.State,
+): Promise<typeof StateAnnotation.Update> {
+  const llm = new ChatOpenAI({
+    apiKey: process.env.OPENAI_KEY,
+    model: "gpt-4.1-nano",
+  });
+  const { data, analysis, charts, goals } = state;
+  const copyWritter = await createCopyWriterAgent({ llm: llm });
+  const finalReport = await copyWritter.invoke({
+    data: data,
+    analysis: analysis,
+    charts: charts,
+    goals: goals,
+  });
+  return {
+    reportMarkdown: finalReport,
+  };
+}
+
 const workflow = new StateGraph(StateAnnotation)
   .addNode("prepareData", PrepareDataNode)
   .addNode("analize", AnalyzeNode)
   .addNode("chartsNode", ChartsNode)
+  .addNode("compile", CompileNode)
   .addEdge("__start__", "prepareData")
   .addEdge("prepareData", "analize")
   //TODO:Would be awesome to have a node to apply critical thinking on the analisis
   .addEdge("prepareData", "chartsNode")
   //TODO:COMPILE NODE we need a node that compiles charts + analisis in a nice markdown format
   //TODO:PUBLISH NODE we need a node that publishes markdown to Notion Blocks in a new page
-  .addEdge("chartsNode", "__end__")
-  .addEdge("analize", "__end__")
+  .addEdge("chartsNode", "compile")
+  .addEdge("analize", "compile")
   .compile();
 
 const testResponse = await workflow.invoke({});
