7cc65a3913f4f8393d390b9dac77335f0f2c8396
i2phi <jfiturbe@gmail.com>
2025-05-19 18:33:30 -0500

    helper function to get user data from db

---
+++ utils/getUserData.ts
@@ -0,0 +1,31 @@
+import { sbc } from "./SupabaseClient";
+
+export interface UserConfig {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  user_id: string;
+  notion_access_token: string;
+  ga_access_token: string;
+  ga_refresh_token: string;
+  ga_property_id: string;
+  frequency: string;
+  report_types: {
+    user: boolean;
+    country: boolean;
+    analytics: boolean;
+  };
+  next_run: string;
+}
+
+export async function getUserData(): Promise<UserConfig | null> {
+  let { data: user_configs, error } = await sbc
+    .from("user_configs")
+    .select("*");
+
+  if (user_configs && user_configs.length > 0) {
+    //console.log(user_configs);
+    return user_configs[0] as UserConfig;
+  }
+  return null;
+}
